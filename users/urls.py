abe9c961ca3f6bedb9e872eb0e1ee399cc58cc58
yurisjbc23 <yurisbellys23@gmail.com>
2022-06-01 15:00:17 -0400

    Retrieve and Update my user profile

--- users/urls.py
+++ users/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('follow/unfollow/user/<int:pk>/', FollowUnfollow.as_view(), name = 'follows_create_delete'),
     path('profile/detail/user/', MyProfileRetrieve.as_view(), name = 'profile_detail'),
     path('profile/detail/user/<int:pk>/', OtherProfileRetrieve.as_view(), name = 'other_profile_detail'),
+    path('update/profile/', MyProfileUpdate.as_view(), name = 'my_profile_detail_update'),
     
 ]
